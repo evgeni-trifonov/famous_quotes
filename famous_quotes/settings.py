1e8e9bb8698746925d47942e776986c10166e6e2
evgeni-trifonov <evt@Tisha.localdomain>
2025-09-02 17:19:07 +0500

    Add settings

--- famous_quotes/settings.py
+++ famous_quotes/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x3hy06%m%)%c$w1e3)!#2scg_0_&a$%l25ekt9&+dpb(m2kv+9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['evgeni.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -118,7 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']  # для разработки
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Важная строка!
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
